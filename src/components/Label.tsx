446e873d296968f32d3d95845edd6694a3bc38af
javiipap <jpiopadilla@gmail.com>
2024-02-18 22:39:17 +0000

    Fix: Form dark mode

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -9,7 +9,7 @@ export default function Label({
 }) {
   return (
     <div className="space-y-2">
-      <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+      <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-neutral-100">
         {label}
       </label>
       {children}
