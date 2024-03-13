7f8270b5d0ea80875be8ea8fd893c5f610b58f68
javiipap <jpiopadilla@gmail.com>
2024-03-13 17:40:10 +0000

    Fix: Add connection closing + getVoters

--- src/db/helpers.ts
+++ src/db/helpers.ts
@@ -1,29 +1,73 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '@/db/schema';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 
-const getConnection = () => {
+const execQuery = async <T>(
+  fn: (db: PostgresJsDatabase<typeof schema>) => Promise<T>
+) => {
   const client = postgres('postgres://pi:password@192.168.1.10:5432/tfg');
   const db = drizzle(client, { schema });
 
-  return db;
+  const result = await fn(db);
+
+  client.end();
+
+  return result;
 };
 
 export const getBallot = async (slug: string) => {
-  const db = getConnection();
-
-  const ballot = await db.query.ballots.findFirst({
-    where: eq(schema.ballots.slug, slug),
-  });
-
-  return ballot;
+  return await execQuery((db) =>
+    db.query.ballots.findFirst({
+      where: eq(schema.ballots.slug, slug),
+    })
+  );
 };
 
 export const getBallots = async () => {
-  const db = getConnection();
+  return await execQuery((db) => db.query.ballots.findMany());
+};
 
-  const ballots = await db.query.ballots.findMany();
-
-  return ballots;
+export const getVoters = async (slug: string) => {
+  return await execQuery((db) =>
+    db
+      .selectDistinctOn([schema.users.id], {
+        id: schema.users.id,
+        name: schema.users.name,
+        hasVoted: schema.votes.id,
+        groupId: schema.userGroups.id,
+        groupName: schema.userGroups.name,
+      })
+      .from(schema.ballots)
+      .where(eq(schema.ballots.slug, slug))
+      .innerJoin(
+        schema.authorizedGroups,
+        eq(schema.ballots.id, schema.authorizedGroups.ballotId)
+      )
+      .innerJoin(
+        schema.userGroups,
+        eq(schema.authorizedGroups.groupId, schema.userGroups.id)
+      )
+      .innerJoin(
+        schema.userGroupMemberships,
+        eq(schema.userGroups.id, schema.userGroupMemberships.groupId)
+      )
+      .innerJoin(
+        schema.users,
+        eq(schema.userGroupMemberships.userId, schema.users.id)
+      )
+      .leftJoin(
+        schema.votes,
+        and(
+          eq(schema.users.id, schema.votes.userId),
+          eq(schema.ballots.id, schema.votes.ballotId)
+        )
+      )
+      .groupBy(
+        schema.votes.id,
+        schema.users.id,
+        schema.userGroups.id,
+        schema.userGroups.name
+      )
+  );
 };
