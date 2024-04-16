872d427326e9fbe08c3212fdce655b6e8b32f75e
javiipap <jpiopadilla@gmail.com>
2024-04-16 23:33:57 +0100

    Feat: Add wasm library for intensive cryptographic operations

---
+++ src/lib/wasm/src/ballot.rs
@@ -0,0 +1,53 @@
+use std::error::Error;
+
+use elastic_elgamal::app::{ChoiceParams, EncryptedChoice, SingleChoice};
+use elastic_elgamal::group::Ristretto;
+use elastic_elgamal::{Ciphertext, Keypair, PublicKey};
+use rand::thread_rng;
+use serde_json;
+
+pub fn generate_elgamal_keypair() -> (Vec<u8>, Vec<u8>) {
+    let mut rng = thread_rng();
+    let key_pair = Keypair::<Ristretto>::generate(&mut rng);
+
+    (
+        Vec::from(key_pair.public().as_bytes()),
+        Vec::from(*key_pair.secret().expose_scalar().as_bytes()),
+    )
+}
+
+pub fn encrypt_vote(pub_key_bytes: Vec<u8>, choice: usize, options_count: usize) -> String {
+    let rng = &mut thread_rng();
+    let receiver = PublicKey::<Ristretto>::from_bytes(&pub_key_bytes).unwrap();
+    let params = ChoiceParams::single(receiver, options_count);
+    let ballot = EncryptedChoice::single(&params, choice, rng);
+
+    serde_json::to_string(&ballot).unwrap()
+}
+
+pub fn sum_ballot_option(acc: String, ballot: String) -> Result<String, Box<dyn Error>> {
+    let parsed_ballot: EncryptedChoice<Ristretto, SingleChoice> = serde_json::from_str(&ballot)?;
+    let mut parsed_acc: Vec<Ciphertext<Ristretto>> = serde_json::from_str(&acc)?;
+
+    assert_eq!(parsed_ballot.len(), parsed_acc.len());
+
+    for (i, choice) in parsed_ballot.choices_unchecked().iter().enumerate() {
+        parsed_acc[i] += *choice;
+    }
+
+    Ok(serde_json::to_string(&parsed_acc)?)
+}
+
+pub fn verify_vote(
+    pub_key_bytes: Vec<u8>,
+    vote: String,
+    options_count: usize,
+) -> Result<(), Box<dyn Error>> {
+    let receiver = PublicKey::<Ristretto>::from_bytes(&pub_key_bytes)?;
+    let params = ChoiceParams::single(receiver, options_count);
+    let ballot: EncryptedChoice<Ristretto, SingleChoice> = serde_json::from_str(&vote)?;
+
+    ballot.verify(&params)?;
+
+    Ok(())
+}
---
+++ src/lib/wasm/src/lib.rs
@@ -0,0 +1,99 @@
+mod ballot;
+mod blind_signature;
+
+pub use ballot::*;
+pub use blind_signature::*;
+
+#[cfg(test)]
+mod tests {
+    use elastic_elgamal::{
+        app::{ChoiceParams, EncryptedChoice, SingleChoice},
+        group::Ristretto,
+        Ciphertext, DiscreteLogTable, PublicKey, SecretKey,
+    };
+
+    use super::*;
+
+    pub fn tally(pub_key_bytes: Vec<u8>, votes: Vec<String>, options_count: usize) -> String {
+        let receiver = PublicKey::<Ristretto>::from_bytes(&pub_key_bytes).unwrap();
+        let params = ChoiceParams::single(receiver, options_count);
+        let mut result =
+            serde_json::to_string(&vec![Ciphertext::<Ristretto>::zero(); options_count]).unwrap();
+
+        votes.iter().for_each(|v| {
+            let parsed: EncryptedChoice<Ristretto, SingleChoice> = serde_json::from_str(v).unwrap();
+
+            match parsed.verify(&params) {
+                Ok(_) => result = sum_ballot_option(result.clone(), v.clone()).unwrap(),
+                Err(_) => (),
+            }
+        });
+
+        result
+    }
+
+    #[test]
+    fn e2e_election_works() {
+        let (pub_key_bytes, secret_key_bytes) = generate_elgamal_keypair();
+        let encrypted: Vec<String> = vec![
+            encrypt_vote(pub_key_bytes.clone(), 2, 5),
+            encrypt_vote(pub_key_bytes.clone(), 3, 5),
+            encrypt_vote(pub_key_bytes.clone(), 4, 5),
+            encrypt_vote(pub_key_bytes.clone(), 3, 5),
+            encrypt_vote(pub_key_bytes.clone(), 3, 5),
+            encrypt_vote(pub_key_bytes.clone(), 3, 5),
+        ];
+
+        let res = tally(pub_key_bytes, encrypted, 5);
+
+        let parsed: Vec<Ciphertext<Ristretto>> = serde_json::from_str(&res).unwrap();
+
+        let secret_key = SecretKey::<Ristretto>::from_bytes(&secret_key_bytes).unwrap();
+
+        let lookup_table = DiscreteLogTable::new(0..20);
+
+        let decrypted = parsed
+            .iter()
+            .map(|candidate| secret_key.decrypt(*candidate, &lookup_table))
+            .collect::<Vec<_>>();
+
+        println!("Final tally: {:?}", decrypted);
+    }
+
+    #[test]
+    fn verify_vote_works() {
+        let (pub_key_bytes, _) = generate_elgamal_keypair();
+        let pub_key = PublicKey::<Ristretto>::from_bytes(&pub_key_bytes).unwrap();
+        let params = ChoiceParams::single(pub_key, 5);
+        let encrypted: EncryptedChoice<Ristretto, SingleChoice> =
+            serde_json::from_str(&encrypt_vote(pub_key_bytes, 2, 5)).unwrap();
+
+        encrypted.verify(&params).unwrap();
+    }
+
+    #[test]
+    fn blind_signature_works() {
+        let key_pair = generate_rsa_keypair().unwrap();
+        println!("Generated key pair");
+
+        let msg = Vec::from(b"Hola que tal");
+        let req = create_request(key_pair.0.clone(), msg.clone()).unwrap();
+        println!("Created request");
+
+        let blind_signature = sign(key_pair.1, req.blind_msg.0).unwrap();
+        println!("Signed request");
+
+        let signature = unblind(
+            key_pair.0.clone(),
+            msg.clone(),
+            req.secret.0,
+            *req.msg_randomizer.unwrap(),
+            blind_signature.0,
+        )
+        .unwrap();
+
+        println!("Signature: {:?}", signature.0);
+
+        let _ = verify(key_pair.0, signature.0, *req.msg_randomizer.unwrap(), msg);
+    }
+}
