9e5aa0317a2d8cb8c3e3dc7a9a1c13245953cba3
javiipap <jpiopadilla@gmail.com>
2024-04-27 10:19:38 +0100

    Fix: Divided lib in two

--- src/lib/ballots/src/lib.rs
+++ src/lib/ballots/src/lib.rs
@@ -1,8 +1,6 @@
 mod ballot;
-mod blind_signature;
 
 pub use ballot::*;
-pub use blind_signature::*;
 
 #[cfg(test)]
 mod tests {
@@ -69,31 +67,5 @@ mod tests {
             serde_json::from_str(&encrypt_vote(pub_key_bytes, 2, 5)).unwrap();
 
         encrypted.verify(&params).unwrap();
-    }
-
-    #[test]
-    fn blind_signature_works() {
-        let key_pair = generate_rsa_keypair().unwrap();
-        println!("Generated key pair");
-
-        let msg = Vec::from(b"Hola que tal");
-        let req = create_request(key_pair.0.clone(), msg.clone()).unwrap();
-        println!("Created request");
-
-        let blind_signature = sign(key_pair.1, req.blind_msg.0).unwrap();
-        println!("Signed request");
-
-        let signature = unblind(
-            key_pair.0.clone(),
-            msg.clone(),
-            req.secret.0,
-            *req.msg_randomizer.unwrap(),
-            blind_signature.0,
-        )
-        .unwrap();
-
-        println!("Signature: {:?}", signature.0);
-
-        let _ = verify(key_pair.0, signature.0, *req.msg_randomizer.unwrap(), msg);
     }
 }
