1b3eae8e14ecfdbbffe392ccefed85086bd9a691
javiipap <jpiopadilla@gmail.com>
2024-04-30 10:25:21 +0100

    Feat: Update env variables and add t3 env

---
+++ src/env.ts
@@ -0,0 +1,23 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    DB_USER: z.string(),
+    DB_PWD: z.string(),
+    DB_NAME: z.string(),
+    DB_HOST: z.string(),
+    DB_PORT: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    AUTH_SECRET: z.string(),
+    ETH_ACCOUNT: z.string(),
+    ETH_PRIV: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_RSA_PUBLIC: z.string(),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_RSA_PUBLIC: process.env.NEXT_PUBLIC_RSA_PUBLIC,
+  },
+});
