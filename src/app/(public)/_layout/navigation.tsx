28957784e2a12c668e1d61b90a057729d1a7f992
javiipap <jpiopadilla@gmail.com>
2024-05-20 18:09:45 +0100

    Feat: Responsive header

--- src/app/(public)/_layout/navigation.tsx
+++ src/app/(public)/_layout/navigation.tsx
@@ -1,35 +1,59 @@
 'use client';
 
 import { signOutAction } from '@/app/(public)/_layout/actions';
+import { Menu } from 'lucide-react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Navigation({ user }: { user: any | null }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="">
-      <ul className="flex space-x-4">
+      <ul
+        className={`${
+          isOpen ? 'left-0' : 'left-[-100vw]'
+        } transition-all fixed h-full w-full bg-background top-[80px] ml-12 mt-8 text-xl space-y-8 flex flex-col md:left-0 md:relative md:top-0 md:text-base md:mt-0 md:space-y-0 md:ml-0 md:flex-row md:space-x-4`}
+      >
         <li>
           <a href="/">Home</a>
         </li>
         {user ? (
           <>
             <li>
-              <Link href="/dashboard">Dashboard</Link>
+              <Link href="/dashboard" onClick={() => setIsOpen(false)}>
+                Dashboard
+              </Link>
             </li>
-            <li className="cursor-pointer" onClick={() => signOutAction({})}>
+            <li
+              className="cursor-pointer"
+              onClick={() => {
+                signOutAction({});
+                setIsOpen(false);
+              }}
+            >
               Sign out
             </li>
           </>
         ) : (
           <>
             <li>
-              <Link href="/login">Login</Link>
+              <Link href="/login" onClick={() => setIsOpen(false)}>
+                Login
+              </Link>
             </li>
             <li>
-              <Link href="/register">Register</Link>
+              <Link href="/register" onClick={() => setIsOpen(false)}>
+                Register
+              </Link>
             </li>
           </>
         )}
       </ul>
+      <Menu
+        className="md:hidden cursor-pointer"
+        onClick={() => setIsOpen((v) => !v)}
+      />
     </nav>
   );
 }
