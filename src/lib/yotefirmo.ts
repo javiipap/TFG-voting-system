88a76d536dc02889677291997bfba0cb96d5be63
javiipap <jpiopadilla@gmail.com>
2025-07-08 20:47:23 +0100

    fix: Implement retry opening the socket

--- src/lib/yotefirmo.ts
+++ src/lib/yotefirmo.ts
@@ -1,23 +1,37 @@
+import { retry } from '@/lib/utils';
+
+const openSocket = async (socketAddr: string) => {
+  const socket = new WebSocket('ws://localhost:9999');
+  await new Promise((res, rej) => {
+    socket.addEventListener('error', rej);
+    socket.addEventListener('open', res);
+  });
+
+  return socket;
+};
+
 export async function requestUserInteraction(
   action: 'encrypt' | 'sign',
-  payload: Buffer
+  payload: Buffer,
+  maxRetries: number = 5
 ) {
   if (!window) {
     throw 'This function is browser only';
   }
 
-  window.location.href = 'yotefirmo://';
-  const socket = new WebSocket('ws://localhost:9999');
+  // window.location.href = 'yotefirmo://';
+  const socket = await retry(
+    () => openSocket('ws://localhost:9999'),
+    maxRetries
+  );
 
   return await new Promise<string>((resolve) => {
-    socket.addEventListener('open', (_evt) => {
-      socket.send(
-        JSON.stringify({ action, payload: payload.toJSON().data }, null, 0)
-      );
+    socket.send(
+      JSON.stringify({ action, payload: payload.toJSON().data }, null, 0)
+    );
 
-      socket.addEventListener('message', (evt) => {
-        resolve(evt.data);
-      });
+    socket.addEventListener('message', (evt) => {
+      resolve(evt.data);
     });
   });
 }
