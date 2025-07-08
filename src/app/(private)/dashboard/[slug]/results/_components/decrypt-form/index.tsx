d786c984793c8771b64e0b179601780ab3a22cc6
javiipap <jpiopadilla@gmail.com>
2025-07-08 20:50:24 +0100

    feat: Implement shamir secret sharing

--- src/app/(private)/dashboard/[slug]/results/_components/decrypt-form/index.tsx
+++ src/app/(private)/dashboard/[slug]/results/_components/decrypt-form/index.tsx
@@ -3,7 +3,6 @@
 import { Context } from '@/app/(private)/dashboard/[slug]/context';
 import { storeClearResultsAction } from '@/app/(private)/dashboard/[slug]/results/_components/decrypt-form/actions';
 import LoadingButton from '@/components/loading-button';
-import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import wasm_init, { decrypt_result } from 'client_utilities';
@@ -11,28 +10,46 @@ import { useAction } from 'next-safe-action/hooks';
 import { useContext, useState } from 'react';
 
 export default function DecryptForm() {
-  const { encryptedResult, candidates, slug } = useContext(Context) as Context;
+  const { encryptedResult, candidates, slug, adminCount } = useContext(
+    Context
+  ) as Context;
+  console.log(adminCount);
   const { execute, status } = useAction(storeClearResultsAction);
-  const [inputState, setInputState] = useState('');
+  const [inputState, setInputState] = useState(new Array(adminCount).fill(''));
   const [isLoading, setIsLoading] = useState(false);
 
   const decryptResult = async () => {
     setIsLoading(true);
     await wasm_init();
+    try {
+      const input = Object.values(inputState).reduce((acc, share) => {
+        const parsedShare = Buffer.from(share, 'base64');
+        const buf = Buffer.alloc(acc.length + parsedShare.length);
+        buf.set(acc, 0);
+        buf.set(parsedShare, acc.length);
 
-    const result = decrypt_result(
-      Buffer.from(inputState, 'base64') as unknown as Uint8Array,
-      Buffer.from(encryptedResult!.slice(2), 'hex') as unknown as Uint8Array
-    );
+        return new Uint8Array(buf);
+      }, new Uint8Array());
 
-    setIsLoading(false);
-    execute({
-      slug,
-      candidates: candidates.map((c, i) => ({
-        ...c,
-        votes: Number(result[i]),
-      })),
-    });
+      const result = decrypt_result(
+        input,
+        Buffer.from(inputState[0], 'base64').length,
+        adminCount,
+        new Uint8Array(Buffer.from(encryptedResult!.slice(2), 'hex'))
+      );
+
+      setIsLoading(false);
+      execute({
+        slug,
+        candidates: candidates.map((c, i) => ({
+          ...c,
+          votes: Number(result[i]),
+        })),
+      });
+    } catch (e) {
+      console.error(e);
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -43,10 +60,25 @@ export default function DecryptForm() {
       </div>
       <div className="mt-3 space-y-2">
         <Label>Clave privada</Label>
-        <Input
-          value={inputState}
-          onChange={(e) => setInputState(e.target.value)}
-        />
+        <div className="flex flex-wrap gap-4 pb-4">
+          {[...new Array(adminCount).fill(0)].map((_, i) => (
+            <div
+              className="w-full md:w-[calc(50%-theme('spacing.2'))]"
+              key={`private_share-${i}`}
+            >
+              <Input
+                value={inputState[i]}
+                className="mt-1 block w-full"
+                placeholder={`Fragmento ${i + 1}`}
+                onChange={(e) =>
+                  setInputState((inp) =>
+                    inp.map((val, j) => (j === i ? e.target.value : val))
+                  )
+                }
+              />
+            </div>
+          ))}
+        </div>
         <LoadingButton
           onClick={decryptResult}
           disabled={status === 'executing' || isLoading}
